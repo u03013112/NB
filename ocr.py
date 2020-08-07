a8878541fb3803e4b0ef4dd4066338f7be399a67
u03013112@hotmail.com <u03013112@hotmail.com>
2020-08-07 19:36:38 +0800

    check

--- ocr.py
+++ ocr.py
@@ -1,29 +1,33 @@
 # encoding:utf-8
-
 import requests
 import base64
 
-'''
-通用文字识别
-'''
+class OCR:
+    def __init__(self,client_id,client_secret):
+        self.client_id = client_id
+        self.client_secret = client_secret
 
-# client_id 为官网获取的AK， client_secret 为官网获取的SK
-host = 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=dmrLU83szTXVUd5SEQKNQbtu&client_secret=K0N6gTZwaaPeI4nN8Ij2ZEaI5tt1aEuE'
-response = requests.get(host)
-if response:
-    # print(response.json())
-    access_token = response.json().get('access_token')
-    # print(access_token)
+    def parse(self,filename):
+        host = 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id='+self.client_id+'&client_secret='+self.client_secret
+        response = requests.get(host)
+        if response:
+            # print(response.json())
+            access_token = response.json().get('access_token')
+            # print(access_token)
+            if access_token:
+                request_url = "https://aip.baidubce.com/rest/2.0/ocr/v1/general_basic"
+                # 二进制方式打开图片文件
+                f = open(filename, 'rb')
+                img = base64.b64encode(f.read())
 
-    request_url = "https://aip.baidubce.com/rest/2.0/ocr/v1/general_basic"
-    # 二进制方式打开图片文件
-    f = open('pic.png', 'rb')
-    img = base64.b64encode(f.read())
+                params = {"image":img}
+                # access_token = '[调用鉴权接口获取的token]'
+                request_url = request_url + "?access_token=" + access_token
+                headers = {'content-type': 'application/x-www-form-urlencoded'}
+                response = requests.post(request_url, data=params, headers=headers)
+                if response:
+                    print (response.json())
 
-    params = {"image":img}
-    # access_token = '[调用鉴权接口获取的token]'
-    request_url = request_url + "?access_token=" + access_token
-    headers = {'content-type': 'application/x-www-form-urlencoded'}
-    response = requests.post(request_url, data=params, headers=headers)
-    if response:
-        print (response.json())
+if __name__=='__main__':
+    o = OCR('dmrLU83szTXVUd5SEQKNQbtu','K0N6gTZwaaPeI4nN8Ij2ZEaI5tt1aEuE')
+    o.parse('pic.png')
--- pic.py
+++ pic.py
@@ -1,7 +1,8 @@
 #coding=utf-8
-from PIL import ImageGrab
+from PIL import ImageGrab,Image
 import pyautogui
-import cv2
+import base64
+from io import BytesIO
 
 class Pic:
     def capture(self,filename):
@@ -13,8 +14,19 @@ class Pic:
     def captureByAutogui(self,filename):
         im1 = pyautogui.screenshot()
         im1.save(filename)
-
+    def pil2base64(self,image):
+        imgBytesIO = BytesIO()
+        image.save(imgBytesIO, format='JPEG')
+        byte_data = imgBytesIO.getvalue()
+        base64_str = base64.b64encode(byte_data)
+        return base64_str
 
 if __name__=='__main__':
-    p = Pic()
-    p.capture("pic.png")
+    # p = Pic()
+    # p.capture("pic.png")
+    im = pyautogui.screenshot()
+    region = im.crop((50,0,1000,22))
+    region = region.convert('RGB')
+    print(region)
+    pic = Pic()
+    print(pic.pil2base64(region))
