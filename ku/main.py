46f49673feb8921c5bb1d403ca5eb0d638253337
u03013112 <u03013112@hotmail.com>
2020-08-19 01:48:27 +0800

    只有0赚钱，剩下都是亏钱，可能还是代码写错了，明天再查吧

--- ku/main.py
+++ ku/main.py
@@ -8,8 +8,8 @@ if __name__=='__main__':
     n = 40
     p = Prepare()
     data = np.load('../2020-08.npy')
-    p.prepare(data,n,0)
+    p.prepare(data,n,3)
 
-    ai = AI(n,'./models/model_40_0.h5')
+    ai = AI(n,'./models/model_40_3.h5')
     ai.train(p.trainX,p.trainY,p.testX,p.testY)
     ai.test(p.testX,p.testY)
