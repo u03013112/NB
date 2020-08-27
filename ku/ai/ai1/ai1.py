20f955f33f621cc3ff90b5a7f49fd2697f8ab492
u03013112@hotmail.com <u03013112@hotmail.com>
2020-08-27 18:10:59 +0800

    删除大量npy，直接都放到数据库里

--- ku/ai/ai1/ai1.py
+++ ku/ai/ai1/ai1.py
@@ -18,13 +18,13 @@ class AI:
         model = keras.Sequential(
             [
                 keras.Input(shape=(n*10)),
-                keras.layers.Dense(512, activation="relu"),
+                keras.layers.Dense(512, activation="relu",kernel_initializer='zeros',bias_initializer='zeros'),
                 keras.layers.Dropout(0.3),
-                keras.layers.Dense(512, activation="relu"),
+                keras.layers.Dense(512, activation="relu",kernel_initializer='zeros',bias_initializer='zeros'),
                 keras.layers.Dropout(0.3),
-                keras.layers.Dense(512, activation="relu"),
+                keras.layers.Dense(512, activation="relu",kernel_initializer='zeros',bias_initializer='zeros'),
                 keras.layers.Dropout(0.3),
-                keras.layers.Dense(10, activation="softmax"),
+                keras.layers.Dense(10, activation="softmax",kernel_initializer='zeros',bias_initializer='zeros'),
             ]
         )
         model.compile(loss='categorical_crossentropy',optimizer='sgd',metrics=['accuracy'])
@@ -38,7 +38,7 @@ class AI:
             monitor='val_accuracy',
             mode='max',
             save_best_only=True)
-        self.model.fit(trainX, trainY, epochs=50, batch_size=16,validation_split=0.2,callbacks=[model_checkpoint_callback])
+        self.model.fit(trainX, trainY, epochs=30, batch_size=16,validation_split=0.2,callbacks=[model_checkpoint_callback])
         # self.model.save(self.filename)
         return
     def test(self,testX, testY):
@@ -61,7 +61,7 @@ class AI:
         retStr += "totoal come:" + str(come) + "\n"
         retStr += "totoal profit:" + str(come-cost) + "\n"
         print(retStr)
-        return retStr
+        return retStr,come-cost
     def load(self):
         self.model = keras.models.load_model(self.filename)
     def predict(self,data40):
@@ -81,15 +81,19 @@ if __name__=='__main__':
     data = Data()
     
     retStr = ''
+    total = 0.0
     for i in range(10):
         retStr += '\n第'+str(i+1)+'名:\n'
         ai = AI(n,'model'+str(i)+'.h5')
         data.getAll()
         p.prepare(data.data,n,i)
         ai.train(p.trainX,p.trainY,p.testX,p.testY)
-        retStr += ai.test(p.testX,p.testY) + "\n"
+        s,n = ai.test(p.testX,p.testY)
+        retStr += s + "\n"
+        total += n
 
     print(retStr)
+    print('\n总结果：'+n)
     # data.getLatest(200)
     # p.prepare(data.data,n,0)
     # ai.test(p.x,p.y)
